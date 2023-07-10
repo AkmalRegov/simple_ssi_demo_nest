730278fa0ee4a22c5afd70c1480215221af1eb69
AkmalRegov <m.akmal@regovtech.com>
2023-07-10 16:41:38 +0800

    With current code and few manual changes after cloning or installation, managed to issue credential wihout revocation to Orbit Edge wallet.

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ This app aims to accomplish:
 
 - Interaction between mobile wallet agent with custom backend wallet agent.
 - Issuing of verifiable credential from custom backend wallet agent to mobile wallet agent.
-- Revoking of verifiable credential by custom backend wallet agent and reflected on mobile wallet agent.
+- Revoking of verifiable credential by custom backend wallet agent and reflected on mobile wallet agent. (Postponed)
+- Integration with Postgres for credential storage for issuer and especially for holder.
 
 ## Technology used
 
@@ -18,14 +19,43 @@ Mobile wallets used are:
 1. Lissi ID wallet by Neosfer GmbH (https://www.lissi.id/for-users)
 2. Orbit Edge Wallet by Northern Block (https://northernblock.io/orbit-edge-wallet/)
 
+_NOTE_: Lissi wallet is by far the most problematic and would not be recommended to use for this.
+
 Tech stack:
 
 - NestJS
 - Aries Framework Javascript (AFJ)
 
-_Note_: The custom backend wallet agent uses the Aries Askar package for wallet and storage implementation since the Indy SDK package will be deprecated in the near future. (https://aries.js.org/guides/0.4/getting-started/set-up#adding-a-wallet-and-storage-implementation)
+_NOTE_: The custom backend wallet agent uses the Aries Askar package for wallet and storage implementation since the Indy SDK package will be deprecated in the near future. (https://aries.js.org/guides/0.4/getting-started/set-up#adding-a-wallet-and-storage-implementation)
 
-## Important Notes
+## How to run the code (should be really helpful)
 
-- To start ngrok on static domain (Make sure to use the account)
-  $ ngrok http --hostname=akmalregov.ngrok.dev 3333
+### Schema and Credential Definition registration and errors
+The registration and usage of the Schema and Credential Definition is quite tricky mainly because of these reasons stated in [AFJ 0.3.0 Side Notes](https://aries.js.org/guides/0.3/tutorials/issue-a-credential#side-notes), which are:
+1. When attempting to register a credential definition that already exists on the ledger but is not in your wallet, AFJ will throw an error (as opposed to returning the credential definition from the ledger in prior versions)
+2. Attempting to register a new credential definition that is already in the wallet in AFJ will return the stored definition without attempting to register it on the ledger.
+> These choices are intentional. In case 1, it is assumed that this workflow is a mistake. In case 2, it is assumed that having registered the credential on the ledger is implied.
+
+Hence for troubleshooting purposes:
+1. If using Docker and local changes are not persisted (through shutdown of docker app or PC), you **MUST** register the Schema and Credential Definition similar to how you would do it for the first time.
+ - You _cannot reuse_ any registered Schema AND/OR Credential Definition that are not created, stored and persisted locally in your machine AND/OR container. Hence, the problem with using Docker for this.
+2. If you get an error when registering the Schema, usually it is because the Schema has already been registered in the ledger (but maybe not locally stored in your machine AND/OR container.)
+3. Make sure **to not create more than one Credential Definition in your machine AND/OR container**, else there would a problem when issuing credentials!
+ - If you did create more than one credential definition, registering a new schema and ONLY 1 new credential definition for that would solve the problem.
+4. For now, there is no guaranteed way to check if Schema and Credential Definition have been registered within your local machine AND/OR container, without through trial and error testing.
+ - AnonCredsApi methods to check registration of Schema and Credential Definition exist but that searches only within your local machine AND/OR container, hence if local cache and changes are not persisted, you need to register new ones.
+ - 2 methods in AcmeAgent.ts to check registered Schema are checkSchema_LiteralValue and checkSchema_NotLiteralValue.
+  - checkSchema_LiteralValue is a method where you want to check if your previous registered schema (registered elsewhere on other machine AND/OR container) exist within the ledger (but it does not check within your local machine AND/OR container).
+  - checkSchema_NotLiteralValue is a method where you want to check if the schema is registered after your first run of the registration. **This method invocation should be commented out IF you are doing the first time registration for the schema and credential definition.**
+
+### Ngrok usage
+Ngrok is used to expose the localhost of the issuer to the recipient agent. Make sure it is up and running.
+- To start ngrok on static domain (Make sure to use your own ngrok account and domain)
+  ```shell ngrok http --hostname=akmalregov.ngrok.dev 3333 (Mac/Linux) ```
+  ```shell ngrok http --domain=akmalregov.ngrok.dev 3333 (Windows) ```
+
+### Connection establishment and Connection ID
+1. By making a HTTP get request that creates the agent invitation, it should also generate a QR code within the root project directory.
+2. Use the qr code generated, located in the root project directory by scanning it with an Aries agent mobile wallet/app.
+3. Once connection is established, make sure to save the Connection ID logged by the console and used that in the credential issuance request
+along with Credential Definition ID.
--- src/agent/class/AcmeAgent.ts
+++ src/agent/class/AcmeAgent.ts
@@ -42,13 +42,19 @@ import {
   AnonCredsCredentialFormatService,
   AnonCredsModule,
   LegacyIndyCredentialFormatService,
+  V1CredentialProtocol,
+  getUnqualifiedCredentialDefinitionId,
+  parseIndyCredentialDefinitionId,
 } from '@aries-framework/anoncreds';
 import { AnonCredsRsModule } from '@aries-framework/anoncreds-rs';
 import { anoncreds } from '@hyperledger/anoncreds-nodejs';
+import { BCOVRIN_GENESIS_TRANSACTIONS } from 'src/utils';
 
 export class AcmeAgent extends Agent {
+  // What you input on bcovrin. Should be kept secure in production!
   private unqualifiedIndyDid = `9VdxM6gJS8tyDzeHxKBd9e`; // will be returned after registering seed on bcovrin
   private indyDid = `did:indy:bcovrin:test:${this.unqualifiedIndyDid}`;
+  
   constructor() {
     const config: InitConfig = {
       label: 'demo-agent-acme',
@@ -75,10 +81,7 @@ export class AcmeAgent extends Agent {
             {
               isProduction: false,
               indyNamespace: 'bcovrin:test',
-              genesisTransactions: `{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node1","blskey":"4N8aUNHSgjQVgkpm8nhNEfDf6txHznoYREg9kirmJrkivgL4oSEimFF6nsQ6M41QvhM2Z33nves5vfSn9n1UwNFJBYtWVnHYMATn76vLuL3zU88KyeAYcHfsih3He6UHcXDxcaecHVz6jhCYz1P2UZn2bDVruL5wXpehgBfBaLKm3Ba","blskey_pop":"RahHYiCvoNCtPTrVtP7nMC5eTYrsUA8WjXbdhNc8debh1agE9bGiJxWBXYNFbnJXoXhWFMvyqhqhRoq737YQemH5ik9oL7R4NTTCz2LEZhkgLJzB3QRQqJyBNyv7acbdHrAT8nQ9UkLbaVL9NBpnWXBTw4LEMePaSHEw66RzPNdAX1","client_ip":"138.197.138.255","client_port":9702,"node_ip":"138.197.138.255","node_port":9701,"services":["VALIDATOR"]},"dest":"Gw6pDLhcBcoQesN72qfotTgFa7cbuqZpkX3Xo6pLhPhv"},"metadata":{"from":"Th7MpTaRZVRYnPiabds81Y"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fea82e10e894419fe2bea7d96296a6d46f50f93f9eeda954ec461b2ed2950b62"},"ver":"1"}
-              {"reqSignature":{},"txn":{"data":{"data":{"alias":"Node2","blskey":"37rAPpXVoxzKhz7d9gkUe52XuXryuLXoM6P6LbWDB7LSbG62Lsb33sfG7zqS8TK1MXwuCHj1FKNzVpsnafmqLG1vXN88rt38mNFs9TENzm4QHdBzsvCuoBnPH7rpYYDo9DZNJePaDvRvqJKByCabubJz3XXKbEeshzpz4Ma5QYpJqjk","blskey_pop":"Qr658mWZ2YC8JXGXwMDQTzuZCWF7NK9EwxphGmcBvCh6ybUuLxbG65nsX4JvD4SPNtkJ2w9ug1yLTj6fgmuDg41TgECXjLCij3RMsV8CwewBVgVN67wsA45DFWvqvLtu4rjNnE9JbdFTc1Z4WCPA3Xan44K1HoHAq9EVeaRYs8zoF5","client_ip":"138.197.138.255","client_port":9704,"node_ip":"138.197.138.255","node_port":9703,"services":["VALIDATOR"]},"dest":"8ECVSk179mjsjKRLWiQtssMLgp6EPhWXtaYyStWPSGAb"},"metadata":{"from":"EbP4aYNeTHL6q385GuVpRV"},"type":"0"},"txnMetadata":{"seqNo":2,"txnId":"1ac8aece2a18ced660fef8694b61aac3af08ba875ce3026a160acbc3a3af35fc"},"ver":"1"}
-              {"reqSignature":{},"txn":{"data":{"data":{"alias":"Node3","blskey":"3WFpdbg7C5cnLYZwFZevJqhubkFALBfCBBok15GdrKMUhUjGsk3jV6QKj6MZgEubF7oqCafxNdkm7eswgA4sdKTRc82tLGzZBd6vNqU8dupzup6uYUf32KTHTPQbuUM8Yk4QFXjEf2Usu2TJcNkdgpyeUSX42u5LqdDDpNSWUK5deC5","blskey_pop":"QwDeb2CkNSx6r8QC8vGQK3GRv7Yndn84TGNijX8YXHPiagXajyfTjoR87rXUu4G4QLk2cF8NNyqWiYMus1623dELWwx57rLCFqGh7N4ZRbGDRP4fnVcaKg1BcUxQ866Ven4gw8y4N56S5HzxXNBZtLYmhGHvDtk6PFkFwCvxYrNYjh","client_ip":"138.197.138.255","client_port":9706,"node_ip":"138.197.138.255","node_port":9705,"services":["VALIDATOR"]},"dest":"DKVxG2fXXTU8yT5N7hGEbXB3dfdAnYv1JczDUHpmDxya"},"metadata":{"from":"4cU41vWW82ArfxJxHkzXPG"},"type":"0"},"txnMetadata":{"seqNo":3,"txnId":"7e9f355dffa78ed24668f0e0e369fd8c224076571c51e2ea8be5f26479edebe4"},"ver":"1"}
-              {"reqSignature":{},"txn":{"data":{"data":{"alias":"Node4","blskey":"2zN3bHM1m4rLz54MJHYSwvqzPchYp8jkHswveCLAEJVcX6Mm1wHQD1SkPYMzUDTZvWvhuE6VNAkK3KxVeEmsanSmvjVkReDeBEMxeDaayjcZjFGPydyey1qxBHmTvAnBKoPydvuTAqx5f7YNNRAdeLmUi99gERUU7TD8KfAa6MpQ9bw","blskey_pop":"RPLagxaR5xdimFzwmzYnz4ZhWtYQEj8iR5ZU53T2gitPCyCHQneUn2Huc4oeLd2B2HzkGnjAff4hWTJT6C7qHYB1Mv2wU5iHHGFWkhnTX9WsEAbunJCV2qcaXScKj4tTfvdDKfLiVuU2av6hbsMztirRze7LvYBkRHV3tGwyCptsrP","client_ip":"138.197.138.255","client_port":9708,"node_ip":"138.197.138.255","node_port":9707,"services":["VALIDATOR"]},"dest":"4PS3EDQ3dW1tci1Bp6543CfuuebjFrg36kLAUcskGfaA"},"metadata":{"from":"TWwCRQRZ2ZHMJFn9TzLp7W"},"type":"0"},"txnMetadata":{"seqNo":4,"txnId":"aa5e817d7cc626170eca175822029339a444eb0ee8f0bd20d3b0b76e566fb008"},"ver":"1"}`,
+              genesisTransactions: BCOVRIN_GENESIS_TRANSACTIONS,
               connectOnStartup: true,
             },
           ],
@@ -92,6 +95,9 @@ export class AcmeAgent extends Agent {
         }),
         credentials: new CredentialsModule({
           credentialProtocols: [
+            new V1CredentialProtocol({
+              indyCredentialFormat: new LegacyIndyCredentialFormatService(),
+            }),
             new V2CredentialProtocol({
               credentialFormats: [
                 new LegacyIndyCredentialFormatService(),
@@ -126,21 +132,30 @@ export class AcmeAgent extends Agent {
     connectionId: string,
     credentialDefinitionId: string,
   ) => {
+    const parsedCredentialDefinition = parseIndyCredentialDefinitionId(
+      credentialDefinitionId
+    );
+    const unqualifiedCredentialDefinitionId =
+      getUnqualifiedCredentialDefinitionId(
+        parsedCredentialDefinition.namespaceIdentifier,
+        parsedCredentialDefinition.schemaSeqNo,
+        parsedCredentialDefinition.tag
+      );
     const credentialOffer = this.credentials.offerCredential({
       protocolVersion: <never>'v2',
       comment: 'Identity Card',
       connectionId,
       autoAcceptCredential: AutoAcceptCredential.Always,
       credentialFormats: {
-        anoncreds: {
-          credentialDefinitionId,
-          attributes: [
-            { name: 'name', value: 'Jane Doe' },
-            { name: 'age', value: '23' },
-          ],
-        },
+        // anoncreds: {
+        //   credentialDefinitionId,
+        //   attributes: [
+        //     { name: 'name', value: 'Jane Doe' },
+        //     { name: 'age', value: '23' },
+        //   ],
+        // },
         indy: {
-          credentialDefinitionId,
+          credentialDefinitionId: unqualifiedCredentialDefinitionId,
           attributes: [
             { name: 'name', value: 'Jane Doe' },
             { name: 'age', value: '23' },
@@ -152,15 +167,13 @@ export class AcmeAgent extends Agent {
   };
   registerCredentialDefinition = async (schemaResult: any) => {
     if (schemaResult.type === 'AnonCredsSchemaRecord') return;
-    // const unqualifiedIndyDid = `9VdxM6gJS8tyDzeHxKBd9e`; // will be returned after registering seed on bcovrin
-    // const indyDid = `did:indy:bcovrin:test:${unqualifiedIndyDid}`;
     const anonCredsApi = this.modules.anoncreds as AnonCredsApi;
     const credentialDefinitionResult =
       await anonCredsApi.registerCredentialDefinition({
         credentialDefinition: {
           tag: 'default',
           issuerId: this.indyDid,
-          schemaId: schemaResult.schemaState.schemaId,
+          schemaId: schemaResult.schema.schemaState.schemaId,
         },
         options: {},
       });
@@ -178,34 +191,7 @@ export class AcmeAgent extends Agent {
         .credentialDefinitionId,
     );
   };
-  registerSchema = async () => {
-    // const unqualifiedIndyDid = `9VdxM6gJS8tyDzeHxKBd9e`; // will be returned after registering seed on bcovrin
-    // const indyDid = `did:indy:bcovrin:test:${unqualifiedIndyDid}`;
-    const anonCredsApi = this.modules.anoncreds as AnonCredsApi;
-    const schema = {
-      attrNames: ['name', 'age'],
-      issuerId: this.indyDid,
-      name: 'sth',
-      version: '1.0.0',
-    };
-    // var checkSchemaExist = await anonCredsApi.getCreatedSchemas({
-    //   schemaName: schema.name,
-    // });
-    // if (checkSchemaExist) {
-    //   console.log('schema already exists!');
-    //   setTimeout(() => console.log('schema is: \n', checkSchemaExist[0]), 2000);
-    //   setTimeout(async () => {
-    //     const credentialDefinitionId =
-    //       await anonCredsApi.getCreatedCredentialDefinitions({
-    //         schemaId: checkSchemaExist[0].schemaId,
-    //       });
-    //     console.log(
-    //       'credentialDefinitionId is: \n',
-    //       credentialDefinitionId[0].credentialDefinitionId,
-    //     );
-    //   }, 3000);
-    //   return { schema: checkSchemaExist, type: 'AnonCredsSchemaRecord' };
-    // }
+  checkSchema_LiteralValue = async (anonCredsApi: any) => {
     var checkSchemaExist = await anonCredsApi.getSchema(
       `did:indy:bcovrin:test:9VdxM6gJS8tyDzeHxKBd9e/anoncreds/v0/SCHEMA/sth/1.0.0`,
     );
@@ -221,8 +207,45 @@ export class AcmeAgent extends Agent {
           credentialDefinition.credentialDefinitionId,
         );
       }, 3000);
-      return { schema: checkSchemaExist, type: 'AnonCredsSchemaRecord' };
+      return { schema: checkSchemaExist, type: 'AnonCredsSchemaRecord', flag: true };
     }
+    return {schema: 'none', type: 'none', flag: false}
+  }
+  checkSchema_NotLiteralValue = async (anonCredsApi: any, schema: any) => {
+    var checkSchemaExist = await anonCredsApi.getCreatedSchemas({
+      schemaName: schema.name,
+    });
+    if (checkSchemaExist) {
+      console.log('schema already exists!');
+      setTimeout(() => console.log('schema is: \n', checkSchemaExist[0]), 2000);
+      setTimeout(async () => {
+        const credentialDefinitionId =
+          await anonCredsApi.getCreatedCredentialDefinitions({
+            schemaId: checkSchemaExist[0].schemaId,
+          });
+        console.log(
+          'credentialDefinitionId is: \n',
+          credentialDefinitionId[0].credentialDefinitionId,
+        );
+      }, 3000);
+      return { schema: checkSchemaExist, type: 'AnonCredsSchemaRecord', flag: true };
+    }
+    return {schema: 'none', type: 'none', flag: false}
+  }
+  registerSchema = async () => {
+    const anonCredsApi = this.modules.anoncreds as AnonCredsApi;
+    const schema = {
+      attrNames: ['name', 'age'],
+      issuerId: this.indyDid,
+      name: 'sth',
+      version: '1.0.0',
+    };
+
+    var checkSchema_flag = {schema: 'none', type: 'none', flag: false};
+    checkSchema_flag = (await this.checkSchema_LiteralValue(anonCredsApi))
+    // checkSchema_flag = (await this.checkSchema_NotLiteralValue(anonCredsApi, schema))
+    if(checkSchema_flag["flag"]) return {schema: checkSchema_flag["schema"], type: checkSchema_flag["type"]};
+
     const schemaResult = await anonCredsApi.registerSchema({
       schema,
       options: {},
@@ -237,10 +260,8 @@ export class AcmeAgent extends Agent {
   initialDIDSetup = async () => {
     const seed = TypedArrayEncoder.fromString(
       `akmalregov0000000000000000000000`,
-    ); // What you input on bcovrin. Should be kept secure in production!
-    // const unqualifiedIndyDid = `9VdxM6gJS8tyDzeHxKBd9e`; // will be returned after registering seed on bcovrin
-    // const indyDid = `did:indy:bcovrin:test:${unqualifiedIndyDid}`;
-
+    );
+    
     await this.dids.import({
       did: this.indyDid,
       overwrite: true,
